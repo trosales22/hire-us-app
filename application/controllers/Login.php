5a60fe698cdd417380196da86dc45e2485ff1567
Tristan Rosales <tristanrosales22@gmail.com>
2019-07-18 22:25:18 +0800

    create login/landing page..

---
+++ application/controllers/Login.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+	public function index()
+	{
+		$this->load->helper('url');
+		$this->load->view('login_page');
+	}
+}
