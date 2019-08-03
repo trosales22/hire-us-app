be5f7f4bfa1b60c83719180dee696574936f26bc
Tristan Rosales <tristanrosales22@gmail.com>
2019-08-03 20:04:30 +0800

    add talent details..

--- application/models/api/Talents_model.php
+++ application/models/api/Talents_model.php
@@ -11,14 +11,23 @@ class Talents_model extends CI_Model {
 						WHEN 'HOURLY_RATE' THEN 'PER HOUR'
 						WHEN 'DAILY_RATE' THEN 'PER DAY'
 					END as talent_fee_type,
+					IFNULL(B.talent_description, '') as talent_description,
 					A.location,DATE_FORMAT(A.birth_date, '%M %d, %Y') as birth_date,
-					A.email,A.contact_number,A.gender,IFNULL(B.talent_display_photo, '') as talent_display_photo
+					YEAR(CURDATE()) - YEAR(A.birth_date) as age,
+					A.email,A.contact_number,A.gender,IFNULL(B.talent_display_photo, '') as talent_display_photo,
+					GROUP_CONCAT(C.category_id SEPARATOR ', ') as category_ids,
+					GROUP_CONCAT(D.category_name SEPARATOR ', ') as category_names 
 				FROM 
 					talents A 
 				LEFT JOIN 
 					talents_resources B ON A.talent_id = B.talent_id 
+				LEFT JOIN 
+					talents_category C ON A.talent_id = C.talent_id 
+				LEFT JOIN 
+					param_categories D ON C.category_id = D.category_id 
 				WHERE 
 					A.active_flag = ? 
+				GROUP BY A.talent_id 
 				ORDER BY talent_id DESC
 			";
 		
