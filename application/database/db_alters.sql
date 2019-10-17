beeeb0cb925cb9309fbec26f2f2a03ac2b3aa2b1
Tristan Rosales <tristanrosales0@gmail.com>
2019-10-17 23:19:08 +0800

    Include database file.

--- application/database/db_alters.sql
+++ application/database/db_alters.sql
@@ -1,6 +1,1 @@
--- 10/06/2019
-ALTER TABLE `dbhireus`.`talents_address` 
-CHANGE COLUMN `province` `province` VARCHAR(50) NOT NULL ,
-CHANGE COLUMN `city_muni` `city_muni` VARCHAR(50) NOT NULL ,
-CHANGE COLUMN `barangay` `barangay` VARCHAR(50) NOT NULL ,
-CHANGE COLUMN `zip_code` `zip_code` VARCHAR(50) NOT NULL ;
+-- ALTERS
