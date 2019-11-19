4ab3f7df870f170a1572861a85e5155800762598
Tristan Rosales <tristanrosales0@gmail.com>
2019-11-19 21:49:46 +0800

    Modify db alters.

--- application/database/db_alters.sql
+++ application/database/db_alters.sql
@@ -1,15 +1,15 @@
 -- ALTERS
 
+-- 10/30/2019 (DONE)
 ALTER TABLE `temp_booking_list` 
 CHANGE COLUMN `temp_payment_option` `temp_payment_option` ENUM('BANK_TRANSFER', 'BANK_DEPOSIT') NOT NULL;
 
--- 10/30/2019
 ALTER TABLE `client_booking_list` 
 ADD COLUMN `preferred_venue` LONGTEXT NOT NULL AFTER `preferred_time`;
 
 ALTER TABLE `temp_booking_list` 
 ADD COLUMN `temp_booking_venue` LONGTEXT NOT NULL AFTER `temp_booking_time`;
 
--- 11/18/2019
+-- 11/18/2019 (DONE)
 ALTER TABLE `client_reviews` 
 CHANGE COLUMN `review_rating` `review_rating` VARCHAR(10) NOT NULL;
